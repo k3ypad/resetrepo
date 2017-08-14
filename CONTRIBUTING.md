e2855b26eae0a7355c7572461c5dcb240ad61605
Ben Doyle <benjamin.doyle@ensighten.com>
2017-08-14 16:48:22 +0100

    initial commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,1 @@
+https://trello.com/b/99AvsyXt/resetrepo
---
+++ reset_repo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Variables
+TRASH_PATH="~/.Trash"
+
+if [ -z "$GITHUB_ORG" ]; then
+    sudo shutdown -h now
+fi
+
+repo=${PWD##*/}
+
+cd ..
+
+mv $repo ~/$TRASH_PATH/$repo$date
+
+git clone git@github.com:$GITHUB_ORG/$repo.git
+
+cd $repo
